9f872ba50a269011dbb7fdcd601b373c3ea559f1
Jörg Benke <jbenke@uni-hamburg.de>
2018-01-12 19:11:42 +0100

    Correction of faulty pull

--- midpointrule_mpi_error_criterion.c
+++ midpointrule_mpi_error_criterion.c
@@ -4,33 +4,42 @@
 // Measurement time: job wide with "time"
 
 //
-// IO server machen 
-// MPI_Allreduce und Abbruch bzgl fehlerkriterium
-// aufteilung der intervalle so, dass man nicht nur gleiche anzahl besitzt
+// OK - IO server machen 
+// OK - MPI_Allreduce und Abbruch bzgl fehlerkriterium
+// OK - aufteilung der intervalle so, dass man nicht nur gleiche anzahl besitzt
 // openmp
-// thread-binding (nur Openmp-Basis)
-// process binding
+// - thread-binding (nur Openmp-Basis)
+// OK - process binding
 // kurz intel compiler
 
- 
+// bem:
+// - float schneller als double (>= 2) schneller als long double (>= 5/4)
+// - cast in code ist besser/schneller als impliiter cast 
+// - output (stdout, datei) ist teuer (stdout ca. 1/3 mehr an zeit!?)
+// - nutze process bindung: 
+//   - map-by (round robin strategie), 
+//   - bind-to (wie soll die prozessbindung statt finden 
+//   - rank-by (wie soll die durchnummerierng der Prozesses gemacht werden)
+
+
 #include <math.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define M_PI 3.14159265358979323
 
 unsigned int size, rank;
 unsigned long int nr_of_intervalls = 0;
 unsigned long int iter = 0, nr_of_iterations = 1000000;  
-//unsigned long int mflops = 0, max_mflops = 0, min_mflops = 0;
 unsigned long int increment = 0, rank_increment = 0;
 
 double sum = 0.0, my_pi = 0.0, pi = 0.0, error = 10.0; 
 double x = 0.0, h = 0.0;
 double tolerance = 0.0;
 double mflops = 0.0, max_mflops = 0.0, min_mflops = 0.0;
-double begin, end, begin_loop, end_loop;
+double begin, end;
 
 
 int main( int argc, char **argv )
@@ -39,7 +48,10 @@ int main( int argc, char **argv )
   char string[ 100 ];
   char *ptr;
 
-    MPI_Status *status;
+  unsigned long int i = 0, j = 0;
+  
+  MPI_Status *status;
+
   
   MPI_Init( &argc, &argv );
 
@@ -71,33 +83,24 @@ int main( int argc, char **argv )
   while( ( error > tolerance ) && ( iter < nr_of_iterations ) )
     { 
 
-
-      iter = iter + 1;
+      j= 0;
       sum = 0.0;
 
+      iter = iter + 1;
       increment = iter * size; 
-
-      begin_loop = MPI_Wtime();
-
       h = 1.0 / ( double ) increment;
-      // rank_increment = ( rank + 1 ) * increment;
 
-      // for ( unsigned long int i = rank * increment; i < rank_increment; i++ )
-      for ( unsigned long int i = rank + 1; i <= increment; i += size )
+      for ( i = rank + 1; i <= increment; i += size )
 	{
 
-	  //	x = ( ( double ) i + 0.5 ) * h;
+	  j++;
 	  x = ( ( double ) i - 0.5 ) * h;
 	  sum = sum + ( 4.0 / ( 1.0 + x * x ) ); 
 
 	}
 
       my_pi = sum * h;
-
-      end_loop = MPI_Wtime();
-
-      if ( ( end_loop - begin_loop ) > 0.0002 ) 
-	mflops += ( increment * 6.0 + 2.0 ) / ( pow( 10, 6 ) * ( end_loop - begin_loop ) );
+      mflops = mflops + j * 6.0 + 2.0;
 
       MPI_Allreduce( &my_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
  
@@ -107,7 +110,8 @@ int main( int argc, char **argv )
   
   end = MPI_Wtime();
 
-
+  mflops = mflops / ( pow( 10, 6 ) * ( end - begin ) );
+  
   MPI_Reduce( &mflops, &min_mflops, 1, MPI_DOUBLE, MPI_MIN, 0 , MPI_COMM_WORLD );
   MPI_Reduce( &mflops, &max_mflops, 1, MPI_DOUBLE, MPI_MAX, 0 , MPI_COMM_WORLD );
 
@@ -123,26 +127,25 @@ int main( int argc, char **argv )
       fprintf( fp, "Min. MFLOPS: %10.2g\n", min_mflops );
       fprintf( fp, "Wallclocktime: %f s\n", end - begin );
 
-        fprintf(  fp, "\n" );
-        fprintf(  fp, "MFLOPS: %g\n", mflops );
+      fprintf(  fp, "\n" );
+      fprintf(  fp, "MFLOPS: %g\n", mflops );
         
-        for ( unsigned int i = 1; i < size; i++ )
-               {
-                        MPI_Recv( &mflops, 1, MPI_DOUBLE, i, 99, MPI_COMM_WORLD, status );
-                        fprintf(  fp, "MFLOPS: %g\n", mflops );
+      for ( unsigned int i = 1; i < size; i++ )
+	{
+
+	  MPI_Recv( &mflops, 1, MPI_DOUBLE, i, 99, MPI_COMM_WORLD, status );
+	  fprintf(  fp, "MFLOPS: %g\n", mflops );
                     
-                }
+	}
+
       fclose( fp );
           
     }else{
-        MPI_Send( &mflops, 1, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD );
-      }
+
+    MPI_Send( &mflops, 1, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD );
+
+  }
       
-          
-       
-
-
-
   MPI_Finalize( );
 
   return 0;
